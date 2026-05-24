bf020f4bf2aa3afe073f8906561a939e371147ea
volpeisaac8-stack <volpeisaac8@gmail.com>
2026-05-24 16:22:52 -0400

    commit

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ def pixel_to_geo(x, y, width, height):
     latitude = 90 - (y / height) * 180
     return latitude, longitude
 
+#lat lon to pixel coordinates
+def latlon_to_pixel(lat, lon, width, height):
+    x = (lon + 180) / 360 * width
+    y = (90 - lat) / 180 * height
+    return int(x), int(y)
 
 #loading original random animal data
 animal = get_random_animal()
@@ -34,13 +39,16 @@ animal_image = pygame.image.load(animal["image"])
 animal_image = pygame.transform.scale(animal_image, (250,250))
 
 #check distance and score
-
+distance = 0
 def check_distance(lat,lon):
     global total_score
+    global distance
     guess = (lat, lon)
     correct = (animal["lat"], animal["lon"])
     distance = geodesic(guess, correct).km
     print("Distance from correct location:", distance, "kilometers")
+
+
 
     score = max(0, 5000 - int(distance))
     print("Score:", score)
@@ -87,18 +95,24 @@ while True:
     screen.blit(world_map, (0, 0))
 
     #display animal image and name
-    screen.blit(animal_image, (20,20))
+    screen.blit(animal_image, (20,400))
 
 
     font = pygame.font.SysFont(None, 36)
 
     text = font.render(animal["name"], True, (255,255,255))
 
-    screen.blit(text, (20, 280))
+    screen.blit(text, (20, 380))
 
     #display score
     score_text = font.render(f"Score: {total_score}", True, (255,255,255))
     screen.blit(score_text, (20, 320))
+
+    #display distance
+    message = f"Last Guess Distance: {int(distance)} km"
+    font = pygame.font.SysFont(None, 36)
+    message = font.render(message, True, (255,255,255))
+    screen.blit(message, (20, 350))
 
     pygame.display.update()
     clock.tick(60)
